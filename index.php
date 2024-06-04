ca80c9c226d7a388eec309affeaee50a1a8dc923
Gabimania <gabrielenlo9@gmail.com>
2024-06-04 20:17:33 +0200

    css

--- index.php
+++ index.php
@@ -43,9 +43,7 @@ $results = $stm->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fichajes</title>
-    <!-- Enlace a Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Enlace a tu archivo CSS personalizado -->
     <link rel="stylesheet" href="/assets/css/styles.css">
 </head>
 
